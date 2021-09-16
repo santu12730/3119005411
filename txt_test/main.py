5883eed76b42df71bf39142ed6f2b94a8d55108a
santu12730 <847916483@qq.com>
2021-09-16 20:24:47 +0800

    最终结果，利用命令行参数输入文件路径，把结果输出到一个文件里

--- txt_test/main.py
+++ txt_test/main.py
@@ -1,10 +1,13 @@
 import jieba
 import gensim
 import re
+import sys
+from pip._vendor.distlib.compat import raw_input
+
 
 
 # 获取指定路径的文件内容
-def get_file_contents(path):
+def get_contents(path):
     str = ''
     f = open(path, 'r', encoding='UTF-8')
     line = f.readline()
@@ -13,6 +16,7 @@ def get_file_contents(path):
         line = f.readline()
     f.close()
     return str
+
 
 # 将读取到的文件内容先进行jieba分词，然后再把标点符号、转义符号等特殊符号过滤掉
 def filter(str):
@@ -25,6 +29,7 @@ def filter(str):
             pass
     return result
 
+
 # 传入过滤之后的数据，通过调用gensim.similarities.Similarity计算余弦相似度
 def calc_similarity(text1, text2):
     texts = [text1, text2]
@@ -34,3 +39,19 @@ def calc_similarity(text1, text2):
     test_corpus_1 = dictionary.doc2bow(text1)
     cosine_sim = similarity[test_corpus_1][1]
     return cosine_sim
+
+
+if __name__ == '__main__':
+    path1 = sys.argv[1]  # 论文原文的文件的绝对路径（作业要求）
+    path2 = sys.argv[2]  # 抄袭版论文的文件的绝对路径
+    path3 = sys.argv[3]  # 输出结果绝对路径
+    str1 = get_contents(path1)
+    str2 = get_contents(path2)
+    text1 = filter(str1)
+    text2 = filter(str2)
+    similarity = calc_similarity(text1, text2)
+    print("文章相似度： %.2f" % similarity)
+    # 将相似度结果写入指定文件
+    f = open(path3, 'w', encoding="utf-8")
+    f.write("文章相似度： %.2f" % similarity)
+    f.close()
